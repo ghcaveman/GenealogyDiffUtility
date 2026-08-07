8f7682dea7cd75bb8296633e2aa9b49e0885cc5b
Kevin White <gocaveman@gmail.com>
2026-08-07 07:07:29 -0400

    Added basic form layout

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,6 +1,10 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
 
 namespace GenealogyDiffUtility
 {
@@ -9,6 +13,40 @@ namespace GenealogyDiffUtility
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void OnBrowseLeftClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            await BrowseForFileAsync(LeftFileTextBox, LeftContentTextBlock);
+        }
+
+        private async void OnBrowseRightClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            await BrowseForFileAsync(RightFileTextBox, RightContentTextBlock);
+        }
+
+        private async Task BrowseForFileAsync(TextBox fileNameTextBox, TextBlock contentTextBlock)
+        {
+            var files = await StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Select a file",
+                AllowMultiple = false
+            });
+
+            if (files.Count == 0)
+                return;
+
+            var file = files[0];
+            fileNameTextBox.Text = file.TryGetLocalPath();
+
+            try
+            {
+                contentTextBlock.Text = await File.ReadAllTextAsync(fileNameTextBox.Text!);
+            }
+            catch (Exception ex)
+            {
+                contentTextBlock.Text = $"Failed to read the file:\n{ex.Message}";
+            }
         }
     }
 }
