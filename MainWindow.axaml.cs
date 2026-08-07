0a9c6bb96bd2834556ed330bf7b8e5043eb05199
Kevin White <gocaveman@gmail.com>
2026-08-07 06:19:49 -0400

    Initial commit - blank project

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace GenealogyDiffUtility;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
